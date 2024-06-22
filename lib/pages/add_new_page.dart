cb077c531adf9622dacc7195af13c0a1dd19b1d9
AnupaAlwis <isurajithalwis@gmail.com>
2024-06-22 07:59:01 +0530

    Update

--- lib/pages/add_new_page.dart
+++ lib/pages/add_new_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:workout_planner_full/constants/colors.dart';
 import 'package:workout_planner_full/constants/responsiveness.dart';
+import 'package:workout_planner_full/data/equipment_data.dart';
 import 'package:workout_planner_full/data/exercise_data.dart';
 import 'package:workout_planner_full/data/user_data.dart';
+import 'package:workout_planner_full/models/equipment_model.dart';
 import 'package:workout_planner_full/models/exercise_model.dart';
+import 'package:workout_planner_full/widgets/add_equipment_card.dart';
 import 'package:workout_planner_full/widgets/add_exercise_widget.dart';
 
 class AddNewPage extends StatefulWidget {
@@ -17,6 +20,7 @@ class _AddNewPageState extends State<AddNewPage> {
   final userData = user;
   final exerciseList = ExerciseData().exercisesList;
   final favExerciseList = user.favExerciseList;
+  final equipmentList = EquipmentData().equipmentList;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,32 +60,83 @@ class _AddNewPageState extends State<AddNewPage> {
                   itemBuilder: (context, index) {
                     Exercise exercise = exerciseList[index];
                     return AddExerciseCard(
-                        exerciseTitle: exercise.exerciseName,
-                        exerciseImageUrl: exercise.exerciseImageUrl,
-                        noOfMinutes: exercise.noOfMinutes,
-                        isAdded: userData.exerciseList.contains(exercise), 
-                        toggleAddExercise: () {
-                          setState(() {
-                            if (userData.exerciseList.contains(exercise)){
-                              userData.removeExercise(exercise);
-                            } else {
-                              userData.addExercise(exercise);
-                            }
-                            
+                      exerciseTitle: exercise.exerciseName,
+                      exerciseImageUrl: exercise.exerciseImageUrl,
+                      noOfMinutes: exercise.noOfMinutes,
+                      isAdded: userData.exerciseList.contains(exercise),
+                      toggleAddExercise: () {
+                        setState(() {
+                          if (userData.exerciseList.contains(exercise)) {
+                            userData.removeExercise(exercise);
+                          } else {
+                            userData.addExercise(exercise);
                           }
-                          );
-                        }, toggleAddFavExercise: () {
-                          setState(() {
-                            if(userData.favExerciseList.contains(exercise)){
-                              userData.removeFavExercise(exercise);
-                            }else{
-                              userData.addFavExercise(exercise);
-                            }
-                          });
-                        }, isfavorite: userData.favExerciseList.contains(exercise), );
+                        });
+                      },
+                      toggleAddFavExercise: () {
+                        setState(() {
+                          if (userData.favExerciseList.contains(exercise)) {
+                            userData.removeFavExercise(exercise);
+                          } else {
+                            userData.addFavExercise(exercise);
+                          }
+                        });
+                      },
+                      isfavorite: userData.favExerciseList.contains(exercise),
+                    );
                   },
                 ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "All Equipments",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: kMainBlackColor,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1,
+                    crossAxisSpacing: kDefaultPadding,
+                    mainAxisSpacing: kDefaultPadding,
+                    childAspectRatio: 10 / 7.5),
+                itemCount: equipmentList.length,
+                itemBuilder: (context, index) {
+                  Equipment equipment = equipmentList[index];
+                  return AddEquipmentCard(
+                      equipmentTitle: equipment.equipmentName,
+                      equipmentImageUrl: equipment.equipmentImageUrl,
+                      noOfMinutes: equipment.noOfMinutes,
+                      noOfCalories: equipment.noOfCalories);
+                },
               )
+              // GridView.builder(
+              //   shrinkWrap: true,
+              //   physics: const NeverScrollableScrollPhysics(),
+              //   itemCount: equipmentList.length,
+              //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              //       crossAxisCount: 1,
+              //       mainAxisSpacing: kDefaultPadding,
+              //       crossAxisSpacing: kDefaultPadding,
+              //       childAspectRatio: 1),
+              //   itemBuilder: (BuildContext context, int index) {
+              //     Equipment equipment = equipmentList[index];
+              //     return AddEquipmentCard(
+              //       equipmentTitle: equipment.equipmentName,
+              //       equipmentImageUrl: equipment.equipmentImageUrl,
+              //       noOfMinutes: equipment.noOfMinutes,
+              //       noOfCalories: equipment.noOfCalories);
+              //   },
+              // ),
             ],
           ),
         )),
--- lib/widgets/add_equipment_card.dart
+++ lib/widgets/add_equipment_card.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:workout_planner_full/constants/colors.dart';
 
 class AddEquipmentCard extends StatefulWidget {
-  const AddEquipmentCard({super.key});
+  final String equipmentTitle;
+  final String equipmentImageUrl;
+  final int noOfMinutes;
+  final double noOfCalories;
+
+  const AddEquipmentCard(
+      {super.key,
+      required this.equipmentTitle,
+      required this.equipmentImageUrl,
+      required this.noOfMinutes,
+      required this.noOfCalories});
 
   @override
   State<AddEquipmentCard> createState() => _AddEquipmentCardState();
@@ -12,12 +22,43 @@ class _AddEquipmentCardState extends State<AddEquipmentCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 200,
-      height: 200,
+      width: 100,
       decoration: BoxDecoration(
         color: kSubtitleColor.withOpacity(0.1),
         borderRadius: BorderRadius.circular(10),
       ),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Text(
+          widget.equipmentTitle,
+          style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w800,
+              color: kMainBlackColor),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                widget.equipmentImageUrl,
+                width: 100,
+                fit: BoxFit.cover,
+              ),
+              Text(
+                "${widget.noOfMinutes} min",
+                style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: kMainBlackColor),
+              )
+            ],
+          ),
+        )
+      ]),
     );
   }
 }
